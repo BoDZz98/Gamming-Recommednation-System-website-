4a89bb01c2ebf0088ac8c56366f060c47a6e242a
John <78843355+johnamir40@users.noreply.github.com>
2023-03-17 17:36:11 +0200

    update on the file

--- app/Http/Controllers/SendDataController.php
+++ app/Http/Controllers/SendDataController.php
@@ -9,23 +9,23 @@ class SendDataController extends Controller
 {
     public function processData()
     {
-        // Retrieve data from database
+        // Retrieve data from database.
         $data = DB::table('user_preferences')->get(['user_id']);/* , 'game_id','rating' */
         dump($data);
 
         $data2 = DB::table('user_preferences')->get(['game_id']);/* , 'game_id','rating' */
         dump($data2);
 
-        // Convert data to JSON
+        // Convert data to JSON.
         $json_data = json_encode($data);
         dump($json_data);
 
-        // Execute Python script and pass JSON data as argument
+        // Execute Python script and pass JSON data as argument.
         $command = 'C:\xampp\htdocs\grad_project\Grad_project\bodz.py ' . escapeshellarg($json_data);
         $output = shell_exec($command);
         //dump($output);
 
-        // Handle the output from your Python script as needed
+        // Handle the output from your Python script as needed.
 
         return response()->json(['message' => 'Data processed successfully']);
     }
